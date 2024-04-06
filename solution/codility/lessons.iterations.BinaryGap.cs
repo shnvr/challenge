e1c5b5699419a84a5ac8beb5b19009480597bbed
ico <sahinvr@gmail.com.com>
2024-04-06 16:34:30 +0000

    codility lessons iterations binarygap

---
+++ solution/codility/lessons.iterations.BinaryGap.cs
@@ -0,0 +1,26 @@
+﻿namespace  codility.lessons.iterations;
+using System;
+//https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+public class BinaryGap {
+    public int solution(int N) {
+        // Implement your solution here
+        int max = 0;
+        int current_max = 0;
+        string binary_N = Convert.ToString(N,2);
+        int length = binary_N.Length;
+
+        for(int i = 0; i<length; i++)
+        {
+            if(binary_N[i]=='0')
+            {
+                current_max++;
+            }
+            else
+            {
+                max = current_max>max?current_max:max;
+                current_max = 0;
+            }
+        }
+        return max;
+    }
+}
---
+++ test/codility/lessons.iterations.BinaryGap_Test.cs
@@ -0,0 +1,44 @@
+﻿namespace codility.lessons.iterations;
+using codility.lessons.iterations;
+
+public class BinaryGap_Test
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private BinaryGap _binaryGap;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _binaryGap = new BinaryGap();
+        }
+        //Document
+        [TestCase(9,ExpectedResult = 2)]
+        [TestCase(529,ExpectedResult = 4)]
+        [TestCase(20,ExpectedResult = 1)]
+        //Example
+        [TestCase(1041,ExpectedResult = 5)]
+        [TestCase(15,ExpectedResult = 0)]
+        [TestCase(32,ExpectedResult = 0)]
+        //Correctness
+        [TestCase(1,ExpectedResult = 0)]
+        [TestCase(5,ExpectedResult = 1)]
+        [TestCase(1376796946, ExpectedResult =5)]
+
+        public int CorrectnessTest(int value)
+        {
+            return _binaryGap.solution(value);
+        }
+
+        [TestCase(31,ExpectedResult = 0)]
+        public int PowTest(int value)
+        {
+            int converted_value = (int) Math.Pow(2,value);
+
+            return _binaryGap.solution(converted_value-1);
+        }
+
+    }
+
+}
