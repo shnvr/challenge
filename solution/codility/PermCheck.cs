652ccc9a66508bc8febfa93f371937415bcd6891
ico <sahinvr@gmail.com>
2024-06-15 10:15:14 +0300

    PermCheck added

---
+++ solution/codility/PermCheck.cs
@@ -0,0 +1,39 @@
+using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+public class PermCheck {
+    public int solution(int[] A) {
+        // Implement your solution here
+        int result = 0;
+        int length = A.Length;
+        bool [] perm_map = new bool [length];
+        for(int i = 0; i<length; i++)
+        {
+            int curr = A[i];
+
+            if(curr > length || perm_map[curr-1])
+            {
+                result = 0;
+                break;
+            }
+            else
+            {
+                perm_map[curr-1] = true;
+            }
+        }
+        for(int i = 0; i<length; i++)
+        {
+            if(!perm_map[i])
+            {
+                result = 0;
+                break;
+            }
+            result = 1;
+        }
+        return result;
+    }
+}
